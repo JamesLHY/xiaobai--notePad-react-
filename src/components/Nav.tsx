74a4b096fe57ec91ed2a6e9ac0ed4a3633ce06f3
James <847798512@qq.com>
2020-04-28 18:29:20 +0800

    修复Nav点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
-import {Link} from "react-router-dom";
-import React from "react";
-import Icon from "./icon";
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import React from 'react';
+import Icon from './icon';
 
-const NavWrapper =styled.nav`
+const NavWrapper = styled.nav`
 line-height:24px;
 box-shadow:0 0 3px rgba(0,0,0,0.5);
 > ul {
@@ -11,7 +11,7 @@ display:flex;
  > li{
  width:33.3333%;
  text-align:center;
-
+ > a{
  display: flex;
  flex-direction: column;
  padding: 8px;
@@ -22,26 +22,34 @@ display:flex;
  height: 24px;
  }
  }
+ 
+ 
+ }
 }
 `;
-const Nav =() =>{
-    return(
+const Nav = () => {
+    return (
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name='tag'/>
-                    <Link to="/tags">标签页</Link>
+
+                    <Link to="/tags">
+                        <Icon name='tag'/>
+                        标签页</Link>
                 </li>
                 <li>
-                    <Icon name='rmb'/>
-                    <Link to="/money">记账</Link>
+                    <Link to="/money">
+                        <Icon name='rmb'/>
+                        记账</Link>
                 </li>
                 <li>
-                    <Icon name='chart'/>
-                    <Link to="/statistics">统计</Link>
+
+                    <Link to="/statistics">
+                        <Icon name='chart'/>
+                        统计</Link>
                 </li>
             </ul>
         </NavWrapper>
-    )
-}
-export default Nav
+    );
+};
+export default Nav;
