a486e0408967de8b9308884e193a79225b9e4288
James <847798512@qq.com>
2020-05-11 14:52:08 +0800

    Moneys.tsx 的类型 UI界面

--- src/views/Moneys.tsx
+++ src/views/Moneys.tsx
@@ -45,7 +45,27 @@ font-size: 14px;
  }
 `;
 const CategorySection = styled.section`
-
+font-size: 24px;
+ > ul{
+ display: flex;
+ background: #c4c4c4;
+ > li{
+ width: 50%;
+ text-align: center;
+ padding: 16px 0;
+ position: relative;
+ &.selected::after{
+ content:'';
+ display: block;
+ height: 3px;
+ background: #333;
+ position: absolute;
+ bottom: 0;
+ width: 100%;
+ left: 0;
+ }
+ }
+ }
 `;
 const NumberPadSection = styled.section`
 
@@ -71,7 +91,7 @@ function Moneys() {
             </NotesSection>
             <CategorySection>
                 <ul>
-                    <li>支出</li>
+                    <li className='selected'>支出</li>
                     <li>收入</li>
                 </ul>
             </CategorySection>
