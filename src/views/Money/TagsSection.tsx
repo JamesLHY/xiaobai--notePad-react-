fcaa953f739e3a066d3fb43f8b8cbea092c426c6
James <847798512@qq.com>
2020-05-11 16:24:18 +0800

    完成標簽的選擇和新增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-
-const TagsSection = styled.section`
+import React, {useState} from 'react';
+const Wrapper = styled.section`
 flex-grow: 1;
 background: #ffffff;
 padding: 12px 16px;
@@ -17,6 +17,10 @@ align-items: flex-start;
  padding: 4px 18px;
  font-size: 14px;
  margin: 4px 12px;
+ &.selected{
+ background: grey;
+ color:white
+ }
   }
  }
  > button{
@@ -28,4 +32,34 @@ align-items: flex-start;
  margin-top: 8px;
  }
 `;
+
+const TagsSection: React.FC = () =>{
+    const [tags,setTags] = useState<string[]>(['衣','食','住','行']);
+    const [selectedTag,setSelectedTag] = useState<string[]>([]);
+    const onAddTag=()=>{
+        const tagName = window.prompt('請輸入想添加的標簽名');
+        if(tagName!==null){
+            setTags([...tags,tagName])
+        }
+    };
+    const onToggleTag = (tag:string)=>{
+const index = selectedTag.indexOf(tag)
+        if(index>=0){
+           setSelectedTag(selectedTag.filter(t =>t !==tag))
+        }else{
+            setSelectedTag([...selectedTag, tag])
+        }
+    };
+    return(
+        <Wrapper>
+            <ol>
+                {tags.map(tag =>
+                <li key={tag} onClick={()=>{onToggleTag(tag);}
+                } className={selectedTag.indexOf(tag)>=0 ?'selected' :''}>{tag}</li>
+                )}
+            </ol>
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
+    )
+};
 export {TagsSection}
