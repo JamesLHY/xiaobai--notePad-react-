ad78547aef2816ccf1700a36fa4ee8bcc8b96a4e
James <847798512@qq.com>
2020-05-13 13:43:22 +0800

    数字面板俺就功能完成

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -60,7 +60,15 @@ background: #a9a9a9;
 `;
 
 const NumberPadSection: React.FC = () => {
-    const [output, setOutput] = useState('0');
+    const [output, _setOutput] = useState('0');
+    const setOutput = (output: string) =>{
+        if(output.length >16){
+            output = output.slice(0,16);
+        }else if(output.length ===0){
+            output = '0';
+        }
+        _setOutput(output)
+    }
     const onClickButtonWrapper = (e: React.MouseEvent) => {
         const text = (e.target as HTMLButtonElement).textContent;
         if (text === null) {
@@ -82,6 +90,20 @@ const NumberPadSection: React.FC = () => {
                 } else {
                     setOutput(output + text);
                 }
+                break;
+            case '删除':
+                if(output.length ===1){
+                    setOutput('')
+                }else{
+                    setOutput(output.slice(0,-1))
+                }
+                break;
+            case '清空':
+                setOutput('');
+                break;
+            case '.':
+                if(output.indexOf('.')>=0){return;}
+                setOutput(output +'.');
                 break;
         }
     };
