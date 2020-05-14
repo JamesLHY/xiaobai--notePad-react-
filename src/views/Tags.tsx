0f533ac925f85a9e8c9c612e6804ba22d83b6947
James <847798512@qq.com>
2020-05-14 14:40:24 +0800

    封装findtag到 usetag里

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -44,7 +44,7 @@ function Tags() {
         <Layout>
             <TagList>
                 {tags.map(tag => <li key={tag.id}>
-                    <Link to={'/tags/'+ tag}>
+                    <Link to={'/tags/'+ tag.id}>
                         <span>{tag.name}</span>
                         <Icon name='right'/>
                     </Link>
