f794123599eda613006725985c045dcb865393fb
James <847798512@qq.com>
2020-05-17 14:53:55 +0800

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,8 @@
 yarn build &&
 cd build &&
 git init &&
-ga . &&
-gc . -m 'deploy' &&
+git add . &&
+git commit -m 'deploy' &&
 git remote add origin git@github.com:JamesLHY/xiaobai-notePad-react-version-website.git &&
 git push -u origin master -f
 cd ..
